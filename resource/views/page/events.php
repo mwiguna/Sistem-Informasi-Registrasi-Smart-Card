0eeb69d4993b8e43b1abd2ab95004df9884e9c27
Norman Syarif <norman.syf@gmail.com>
2018-05-01 10:02:38 +0700

    Public registration interface design

--- resource/views/page/events.php
+++ resource/views/page/events.php
@@ -1,52 +1,97 @@
+<nav class="navbar navbar-default">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-offset-1 col-md-10 col-lg-offset-2 col-lg-8">
+				<div class="navbar-header">
+					<a class="navbar-brand" href="<?= url('') ?>">
+						<img src="<?= url('resource/assets/images/logo.png') ?>">
+						<span class="logo-text">SIREG Universitas X</span>
+					</a>
+				</div>
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="<?= url('') ?>">Login</a></li> 
+					<li class="atau">atau</li> 
+					<li><a href="<?= url('daftar') ?>">Daftar</a></li> 
+				</ul>
+			</div>
+		</div>
+	</div>
+</nav>
 
+<div class="main grey">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-lg-offset-2 col-lg-8 col-md-offset-1 col-md-10 section-title">
+				Daftar Event Universitas
+			</div>
 
-<div class="container">
+		</div>
+		<div class="row">
+			<div class="col-lg-offset-2 col-lg-8 col-md-offset-1 col-md-10 panel panel-default content">
 
-	<table class="table">
-		<tr>
-			<td>No.</td>
-			<td>Judul Event</td>
-			<td colspan="2">Organisasi / Lembaga</td>
-		</tr>
+				<?php if(empty($events)): ?>
 
-		<?php
-			$i = 1; foreach($events as $event):
+					<div class="main-box add-reg">
+						<p class="main-message">Registrasi masih kosong.</p>
+					</div>
 
-		    $now   = new DateTime(date('Y-m-d'));
-		    $start = new DateTime($event->start_date);
-		    $end   = new DateTime($event->end_date);
+				<?php else: ?>
 
-			$diffLeft  = $now->diff($end);
-			$diffStart = $now->diff($start);
+					<table class="table table-hover main-table">
+						<thead>
+							<tr>
+								<th class="col-md-1">No.</th>
+								<th class="col-md-5">Judul Event</th>
+								<th class="col-md-3">Organisasi</th>
+								<th class="col-md-3">Keterangan</th>
+							</tr>
+						</thead>
+						<tbody>
+							<?php
+							$i = 1; foreach($events as $event):
 
-			$diffStart = (integer) $diffStart->format( "%R%a");
-			$diffLeft  = (integer) $diffLeft->format( "%R%a");
-		?>
+							$now   = new DateTime(date('Y-m-d'));
+							$start = new DateTime($event->start_date);
+							$end   = new DateTime($event->end_date);
 
-			<tr>
-				<td><?= $i ?></td>
-				<td><?= $event->title ?></td>
-				<td><?= $event->name ?></td>
-				<td>
-					<?php 
-						if($diffStart < 0){
-					 		if($diffLeft < 0){ 
-					 ?>
-							<div>Pendaftaran sudah tutup</div>
+							$diffLeft  = $now->diff($end);
+							$diffStart = $now->diff($start);
 
-							<?php } else { ?>
-				
-							<a href="<?= url('pendaftaran_event/'. Security::encrypt($event->id)) ?>">Daftar</a>
-				
-					<?php } } else { ?>
+							$diffStart = (integer) $diffStart->format( "%R%a");
+							$diffLeft  = (integer) $diffLeft->format( "%R%a");
+							?>
 
-						<div>Pendaftaran dibuka pada <?= date('j F Y', strtotime($event->start_date)) ?></div>
+							<tr>
+								<td><?= $i ?></td>
+								<td><?= $event->title ?></td>
+								<td><?= $event->name ?></td>
+								<td>
+									<?php 
+									if($diffStart < 0){
+										if($diffLeft < 0){ 
+											?>
+											<div style="color: red">Pendaftaran sudah tutup</div>
 
-					<?php } ?>
-				</td>
-			</tr>
+											<?php } else { ?>
 
-		<?php $i++; endforeach; ?>
-	</table>
+											<a href="<?= url('pendaftaran_event/'. Security::encrypt($event->id)) ?>"><button class="button-small-4 button-blue">Daftar</button></a>
 
-</div>
+											<?php } } else { ?>
+
+											<div style="color: green">Pendaftaran dibuka pada <?= date('j F Y', strtotime($event->start_date)) ?></div>
+
+											<?php } ?>
+										</td>
+									</tr>
+
+									<?php $i++; endforeach; ?>
+
+								</tbody>
+							</table>
+
+						<?php endif; ?>
+
+					</div>
+				</div>
+			</div>
+		</div>
--- resource/views/page/register_event.php
+++ resource/views/page/register_event.php
@@ -1,32 +1,55 @@
-
-<div class="container">
-
-	<table class="table">
-		<tr>
-			<td width="25%">Nama Event</td>
-			<td>:</td>
-			<td><?= $event->title ?></td>
-		</tr>
-		<tr>
-			<td>Organisasi / Lembaga Penyelenggara </td>
-			<td>:</td>
-			<td><?= $event->name ?></td>
-		</tr>
-		<tr>
-			<td>Periode Pendaftaran </td>
-			<td>:</td>
-			<td>
-				<?= 
-					date("j F Y", strtotime($event->start_date)) . " - " .
-					date("j F Y", strtotime($event->end_date))
-				?>
-			</td>
-		</tr>
-	</table>
-
-	<div>
-		Scan QR Code dibawah ini lalu tap KTM untuk mendaftar 
-		<img src="<?= url('resource/assets/qrcode/'.basename($key)) ?>.png"">
+<nav class="navbar navbar-default">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-offset-1 col-md-10 col-lg-offset-2 col-lg-8">
+				<div class="navbar-header">
+					<a class="navbar-brand" href="<?= url('') ?>">
+						<img src="<?= url('resource/assets/images/logo.png') ?>">
+						<span class="logo-text">SIREG Universitas X</span>
+					</a>
+				</div>
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="<?= url('') ?>">Login</a></li> 
+					<li class="atau">atau</li> 
+					<li><a href="<?= url('daftar') ?>">Daftar</a></li> 
+				</ul>
+			</div>
+		</div>
 	</div>
+</nav>
 
+<div class="main grey">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-lg-offset-2 col-lg-8 col-md-offset-1 col-md-10 section-title">
+				Pendaftaran Event
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-lg-offset-3 col-lg-6 col-md-offset-2 col-md-8 content">
+				<div class="main-box reg-detail reg-detail-4">
+					<p class="event-title"><?= $event->title ?></p>
+					<table>
+						<tr>
+							<th>Penyelenggara</th>
+							<td>:</td>
+							<td><?= $event->name ?></td>
+						</tr>
+						<tr>
+							<th>Periode Pendaftaran</th>
+							<td>:</td>
+							<td>
+								<?= 
+								date("j F Y", strtotime($event->start_date)) . " - " .
+								date("j F Y", strtotime($event->end_date))
+								?>
+							</td>
+						</tr>
+					</table>
+					<p class="meminta-data">Scan QR Code dibawah ini lalu tap KTM untuk mendaftar </p>
+					<img src="<?= url('resource/assets/qrcode/'.basename($key)) ?>.png"">
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
